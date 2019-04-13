7e10c46b080c9987070553c5c2ec95fea4944858
dh_megane <40923242+dh-megane@users.noreply.github.com>
2019-04-14 01:28:30 +0900

    Update Variables.cs

--- XamAppCenterSample2018/Variables.cs
+++ XamAppCenterSample2018/Variables.cs
@@ -4,7 +4,7 @@ namespace XamAppCenterSample2018
 {
     public static class Variables
     {
-        // NOTE: Replace this example key with a valid subscription key.
+        // //NOTE: Replace this example key with a valid subscription key.
         public static readonly string ApiKey = "TEST";
     }
 }
